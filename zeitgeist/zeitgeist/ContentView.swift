b4bd75d885440fae793aa66235b671d81fefaa80
Jari Pietikäinen <J4R1@Jari-MacBook-Pro.local>
2020-03-19 19:03:10 +0200

    new project with switfUI instead of storyboard

---
+++ zeitgeist/zeitgeist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zeitgeist
+//
+//  Created by Jari Pietikäinen on 19.3.2020.
+//  Copyright © 2020 Z Team. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
