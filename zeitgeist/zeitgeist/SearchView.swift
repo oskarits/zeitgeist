da6f15c1edb4ee98af8d80f07bd225b29f71195e
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-01 17:16:17 +0300

    Toast & Shopping cart

--- zeitgeist/zeitgeist/SearchView.swift
+++ zeitgeist/zeitgeist/SearchView.swift
@@ -11,70 +11,159 @@ import SwiftUI
 struct SearchView: View {
     @ObservedObject var networkingManager = NetworkingManager()
     @State private var searchText : String = ""
+    @State private var itemCart : [String] = []
+    @State private var showPopover: Bool = false
+    @State private var show = false
+    @State private var selectedItem : String = ""
+    @State private var shoppingCartTitleText : String = "Shopping cart"
     
-    var body: some View {
-        VStack(alignment: .leading) {
-            NavigationView{
+    var shoppingCart: some View {
+        NavigationView {
+            VStack {
                 VStack {
-                    SearchBar(text: $searchText, placeholder: "Search items")
-                    List {
-                        ForEach(networkingManager.clothingList.items) { item in
-                            if (self.searchText.isEmpty) {
-                                NavigationLink(destination:
-                                    VStack(alignment: .leading) {
-                                        Text(item.brand).font(.largeTitle)
-                                        Text(item.size)
-                                        Text(item.condition)
-                                        Text(item.description)
-                                        Text("\(item.price) €")
-                                            .font(.system(size: 20))
-                                            .foregroundColor(Color.orange)
-                                    }.onAppear {
-                                        UIApplication.shared.endEditing(true)
-                                }) {
-                                    VStack(alignment: .leading) {
-                                        Text(item.brand)
-                                        Text(item.size)
-                                            .font(.system(size: 11))
-                                            .foregroundColor(Color.gray)
-                                        Text("\(item.price) €")
-                                            .font(.system(size: 11))
-                                            .foregroundColor(Color.orange)
-                                    }
-                                }
-                            }
-                            if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-                                NavigationLink(destination:
-                                    VStack(alignment: .leading) {
-                                        Text(item.brand).font(.largeTitle)
-                                        Text(item.size)
-                                        Text(item.condition)
-                                        Text("\(item.price) €")
-                                            .font(.system(size: 20))
-                                            .foregroundColor(Color.orange)
-                                    }.onAppear {
-                                        UIApplication.shared.endEditing(true)
-                                }) {
-                                    VStack(alignment: .leading) {
-                                        Text(item.brand)
-                                        Text(item.size)
-                                            .font(.system(size: 11))
-                                            .foregroundColor(Color.gray)
-                                        Text("\(item.price) €")
-                                            .font(.system(size: 11))
-                                            .foregroundColor(Color.orange)
+                    Spacer()
+                    HStack(alignment: .center) {
+                        Text("").padding(20)
+                        Spacer()
+                        Text(self.shoppingCartTitleText)
+                            .font(.system(size: 26, weight: .regular))
+                            .foregroundColor(Color.orange)
+                        Spacer()
+                        Button(action: {
+                            self.showPopover.toggle()
+                        }) {
+                            Image(systemName: "return")
+                                .font(Font.system(size: 30, weight: .regular))
+                        }.padding(20)
+                    }
+                    List{
+                        
+                        if itemCart != nil {
+                            ForEach(itemCart, id: \.self) { item in
+                                VStack {
+                                    HStack {
+                                        Text(item)
+                                        Spacer()
+                                        Button(action: {
+                                            self.itemCart = self.itemCart.filter{$0 != item}
+                                            
+                                        }) {
+                                            Image(systemName: "cart.fill.badge.minus")
+                                            .font(Font.system(size: 20, weight: .regular))
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                    .navigationBarItems(leading: Image(systemName: "house").font(Font.system(size: 30, weight: .regular))).navigationBarTitle(Text("Search Items"))
+                }
             }
+        }
+    }
+    
+    var searchNavigation: some View {
+        ForEach(networkingManager.clothingList.items) { item in
+            if (self.searchText.isEmpty) {
+                NavigationLink(destination:
+                    VStack(alignment: .leading) {
+                        Text(item.brand).font(.largeTitle)
+                        Text(item.size)
+                        Text(item.condition)
+                        Text(item.description)
+                        Text("\(item.price) €")
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.orange)
+                    }.onAppear {
+                        UIApplication.shared.endEditing(true)
+                }) {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.brand)
+                                Text(item.size)
+                                    .font(.system(size: 11))
+                                    .foregroundColor(Color.gray)
+                                Text("\(item.price) €")
+                                    .font(.system(size: 11))
+                                    .foregroundColor(Color.orange)
+                            }
+                            Spacer()
+                            Image(systemName: "cart.fill.badge.plus").font(Font.system(size: 22, weight: .regular)).onTapGesture {
+                                self.itemCart.append(item.brand)
+                                self.show.toggle()
+                                print("item added")
+                                self.selectedItem = item.brand
+                            }
+                        }
+                    }
+                }
+            }
+            if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                NavigationLink(destination:
+                    VStack(alignment: .leading) {
+                        Text(item.brand).font(.largeTitle)
+                        Text(item.size)
+                        Text(item.condition)
+                        Text("\(item.price) €")
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.orange)
+                    }.onAppear {
+                        UIApplication.shared.endEditing(true)
+                }) {
+                    VStack(alignment: .leading) {
+                        Text(item.brand)
+                        Text(item.size)
+                            .font(.system(size: 11))
+                            .foregroundColor(Color.gray)
+                        Text("\(item.price) €")
+                            .font(.system(size: 11))
+                            .foregroundColor(Color.orange)
+                    }
+                }
+            }
+        }
+    }
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            NavigationView{
+                VStack {
+                    SearchBar(text: $searchText, placeholder: "Search items")
+                    List {
+                        self.searchNavigation
+                    }
+                }
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .topLeading)
+                    .navigationBarItems( trailing:
+                        Button(action: {
+                            self.showPopover.toggle()
+                        }) {
+                            Image(systemName: "cart.fill")
+                                .font(Font.system(size: 30, weight: .regular))
+                        }
+                )
+                    .navigationBarTitle(Text("Search Items"))
+            }.toast(show: $show, text: selectedItem)
+            ZStack {
+                VStack {
+                    Spacer()
+                    VStack{
+                        VStack {
+                            self.shoppingCart
+                        }
+                    }
+                }
+            }.background(Color.white)
+                .edgesIgnoringSafeArea(.all)
+                .offset(x:0, y: self.showPopover ? 0 : UIApplication.shared.keyWindow?.frame.height ?? 0)
         }.resignKeyboardOnDragGesture()
     }
 }
-
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
@@ -82,3 +171,19 @@ struct SearchView_Previews: PreviewProvider {
     }
 }
 
+
+/*
+ "cart.fill"
+ "cart.fill.badge.plus"
+ "cart.fill.badge.minus"
+ */
+
+   extension Array where Element: Equatable {
+
+    // Remove first collection element that is equal to the given `object`:
+    mutating func remove(object: Element) {
+        guard let index = firstIndex(of: object) else {return}
+        remove(at: index)
+    }
+
+}
