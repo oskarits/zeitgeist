3884737abe9f52b29fabbc9b08b36f6aab35f7de
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-03 13:33:14 +0300

    Single item view struct

--- zeitgeist/zeitgeist/SingleItemView.swift
+++ zeitgeist/zeitgeist/SingleItemView.swift
@@ -2,20 +2,26 @@
 //  SingleItemView.swift
 //  zeitgeist
 //
-//  Created by Jari Pietikäinen on 3.4.2020.
+//  Created by Jari Pietikäinen on 2.4.2020.
 //  Copyright © 2020 Z Team. All rights reserved.
 //
 
 import SwiftUI
 
 struct SingleItemView: View {
+    var item : ClothingListEntry
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SingleItemView_Previews: PreviewProvider {
-    static var previews: some View {
-        SingleItemView()
+            VStack(alignment: .leading) {
+                Text(item.brand).font(.largeTitle)
+                Text(item.size)
+                Text(item.condition)
+                Text(item.description)
+                Text("\(item.price) €")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color.orange)
+            }.onAppear {
+                UIApplication.shared.endEditing(true)
+        }
     }
 }
