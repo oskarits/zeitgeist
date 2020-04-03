a736fe46a2dbe6d92b241024319f3fb3df06a32f
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-03 13:32:22 +0300

    New files for search items

---
+++ zeitgeist/zeitgeist/SingleItemView.swift
@@ -0,0 +1,21 @@
+//
+//  SingleItemView.swift
+//  zeitgeist
+//
+//  Created by Jari Pietikäinen on 3.4.2020.
+//  Copyright © 2020 Z Team. All rights reserved.
+//
+
+import SwiftUI
+
+struct SingleItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SingleItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleItemView()
+    }
+}
