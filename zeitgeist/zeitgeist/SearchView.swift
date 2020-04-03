743094dfe940e40451961cdbdaca49052ac98149
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-03 15:29:44 +0300

    Toggle to add and remove items from shoppinList

--- zeitgeist/zeitgeist/SearchView.swift
+++ zeitgeist/zeitgeist/SearchView.swift
@@ -76,8 +76,16 @@ struct SearchView: View {
                         HStack {
                             ListItem(item: item)
                             Spacer()
-                            Image(systemName: "cart.fill.badge.plus").font(Font.system(size: 22, weight: .regular)).onTapGesture {
-                                self.ShoppingCartPlus(key: item.brand, value: "\(item.id)")
+                            if (self.shoppingList.firstIndex(where: {$0.value == "\(item.id)"}) != nil) {
+                                Image(systemName: "cart.fill.badge.minus").font(Font.system(size: 22, weight: .regular)).onTapGesture {
+                                    self.ShoppingCartMinus(index: "\(item.id)")
+                                }
+                            }
+                            if (self.shoppingList.firstIndex(where: {$0.value == "\(item.id)"}) == nil) {
+                                Image(systemName: "cart.fill.badge.plus").font(Font.system(size: 22, weight: .regular)).onTapGesture {
+                                    self.ShoppingCartPlus(key: item.brand, value: "\(item.id)")
+
+                                }
                             }
                         }
                     }
