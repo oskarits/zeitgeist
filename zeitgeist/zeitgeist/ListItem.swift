14510119255c09fb2628800ccd4c073a86184ed7
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-03 13:32:51 +0300

    List item struct

--- zeitgeist/zeitgeist/ListItem.swift
+++ zeitgeist/zeitgeist/ListItem.swift
@@ -2,20 +2,25 @@
 //  ListItem.swift
 //  zeitgeist
 //
-//  Created by Jari Pietikäinen on 3.4.2020.
+//  Created by Jari Pietikäinen on 2.4.2020.
 //  Copyright © 2020 Z Team. All rights reserved.
 //
 
 import SwiftUI
 
 struct ListItem: View {
+    var item : ClothingListEntry
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ListItem_Previews: PreviewProvider {
-    static var previews: some View {
-        ListItem()
+        
+        VStack(alignment: .leading) {
+            Text(item.brand)
+            Text(item.size)
+                .font(.system(size: 11))
+                .foregroundColor(Color.gray)
+            Text("\(item.price) €")
+                .font(.system(size: 11))
+                .foregroundColor(Color.orange)
+        }
     }
 }
