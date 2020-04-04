e20fea811e914325a1f9b22cfeb0759d86b430a3
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-04 16:09:00 +0300

    Core data and ItemNode class

---
+++ zeitgeist/zeitgeist/Models/ItemNode.swift
@@ -0,0 +1,24 @@
+//
+//  ItemNode.swift
+//  zeitgeist
+//
+//  Created by Jari Pietikäinen on 4.4.2020.
+//  Copyright © 2020 Z Team. All rights reserved.
+//
+
+import CoreData
+
+class ItemNode: NSManagedObject {
+    
+    @NSManaged var id: String?
+    @NSManaged var children: NSSet?
+    @NSManaged var parent: ItemNode?
+}
+
+extension ItemNode {
+    static func getNodes() -> NSFetchRequest<ItemNode> {
+        let request = ItemNode.fetchRequest() as! NSFetchRequest<ItemNode>
+        request.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+        return request
+    }
+}
