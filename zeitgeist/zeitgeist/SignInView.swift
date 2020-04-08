09f825cf7ff191b329c2e5089031a89ff5eb6a58
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2020-04-08 12:11:46 +0300

    styling and added logic to change divider color when tapped

--- zeitgeist/zeitgeist/SignInView.swift
+++ zeitgeist/zeitgeist/SignInView.swift
@@ -14,20 +14,72 @@ struct SignInView: View {
     
     @State var username: String = ""
     @State var password: String = ""
+    @State var isLoggedIn = false
+    
+    @State private var dividerColor = Color.black
+    
     
     var body: some View {
         VStack {
-            Text("Welcome")
+            Text("Sign in")
                 .font(.largeTitle)
                 .foregroundColor(Color.orange)
-            TextField("Username", text: $username)
-            TextField("Password", text: $password)
-            Button(action: {
+                .padding()
+            
+            HStack {
+                Image(systemName: "envelope")
+                VStack {
+                    TextField("Email", text: $username, onEditingChanged: { (editingChanged) in
+                        if editingChanged {
+                            self.dividerColor = Color.orange
+                        } else {
+                            self.dividerColor = Color.black
+                        }
+                    })
+                        .padding()
+                    Divider()
+                        .background(dividerColor)
+                }
                 
+                    
+            }
+            .padding()
+                
+            HStack {
+                Image(systemName: "lock")
+                VStack {
+                    TextField("Password", text: $password, onEditingChanged: { (editingChanged) in
+                        if editingChanged {
+                            self.dividerColor = Color.orange
+                        } else {
+                            self.dividerColor = Color.black
+                        }
+                    })
+                        .padding()
+                    Divider()
+                        .background(dividerColor)
+                }
+                    
+            }
+            .padding()
+            
+           
+                
+
+            Button(action: {
+                self.isLoggedIn = true
+                print("Logged in \(self.isLoggedIn)")
             }) {
                 Text("Sign in")
-            }
-        }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.headline)
+                    .frame(width: 300, height: 80)
+                    .background(Color.orange)
+                    .cornerRadius(15.0)
+                
+            }.padding()
+        }.padding()
     }
 }
 
