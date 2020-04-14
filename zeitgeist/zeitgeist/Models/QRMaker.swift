3584ddb9766f3e40e3cdd250ca34c6b54471a4a8
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-14 10:56:17 +0300

    Creates high res QR image from string

--- zeitgeist/zeitgeist/Models/QRMaker.swift
+++ zeitgeist/zeitgeist/Models/QRMaker.swift
@@ -7,13 +7,42 @@
 //
 
 import SwiftUI
+import CoreData
+import CoreImage.CIFilterBuiltins
+
 
 struct QRMaker: View {
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
+    @FetchRequest(fetchRequest: LoginNode.getNodes()) var isLoggedInResults: FetchedResults<LoginNode>
+    
+    //---
     @State var txt = "testi data"
     
     var body: some View {
         VStack {
-            Image(uiImage: UIImage(data: returnData(str: self.txt))!).resizable().frame(width: 75, height: 75)
+            List {
+                ForEach(isLoggedInResults, id: \.self) { node in
+                    
+                    VStack {
+                        if (node.isLoggedIn) {
+                            Image(uiImage: UIImage(data: self.returnData(str: node.idString))!).interpolation(.none).resizable().frame(width: 150, height: 150)
+                        }
+                        if (node.isLoggedIn == false) {
+                            Image(uiImage: UIImage(data: self.returnData(str: self.txt))!).interpolation(.none).resizable().frame(width: 150, height: 150)
+                        }
+                        Image(uiImage: self.generateQRCode(from: "aaaaaaaaa")).interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        
+                        
+                    }
+                    
+                    
+                }
+            }
         }
     }
     
@@ -26,7 +55,18 @@ struct QRMaker: View {
         let uiImage = UIImage(ciImage: image!)
         return uiImage.pngData()!
     }
-    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
 }
 
 struct QRMaker_Previews: PreviewProvider {
