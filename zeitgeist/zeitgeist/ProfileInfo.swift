c833fb86140271545d24b0308d1d7b8fefcc4ea4
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2020-04-17 13:14:46 +0300

    checks if logged in, if so shows profilepage, if not signIn

---
+++ zeitgeist/zeitgeist/ProfileInfo.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileInfo.swift
+//  zeitgeist
+//
+//  Created by Tom Paavolainen on 17.4.2020.
+//  Copyright © 2020 Z Team. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct ProfileInfo: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
+    @FetchRequest(fetchRequest: LoginNode.getNodes()) var isLoggedInResults: FetchedResults<LoginNode>
+    
+    var body: some View {
+        VStack {
+            Text("John Doe")
+                .font(.largeTitle)
+            Text(isLoggedInResults[0].idString)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            Divider()
+                ReservationView()
+         
+        }.padding()
+        
+    }
+}
+
+struct ProfileInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileInfo()
+    }
+}
