f040371dc0f39d38f2856139612d8c23d6cf4bfb
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-18 04:43:19 +0300

    Simple working size filter

--- zeitgeist/zeitgeist/SearchView.swift
+++ zeitgeist/zeitgeist/SearchView.swift
@@ -21,30 +21,49 @@ struct SearchView: View {
     var dropDown: some View {
         VStack(spacing: 30) {
             HStack {
-                Text("Search by: ")
+                Text("Search by size: ")
                 Image(systemName: expand ? "chevron.up" : "chevron.down")
             }.onTapGesture {
                 self.expand.toggle()
             }
             if expand {
                 Button(action: {
-                    self.searchBy = "size"
+                    self.searchBy = "one"
                     self.expand.toggle()
                 }) {
-                    Text("Size")
+                    Text("One size")
                 }
                 Button(action: {
-                    self.searchBy = "userSize"
+                    self.searchBy = "32"
                     self.expand.toggle()
                 }) {
-                    Text("Your size")
+                    Text("32")
                 }
                 Button(action: {
-                    self.searchBy = "price"
+                    self.searchBy = "34"
                     self.expand.toggle()
                 }) {
-                    Text("Price €")
+                    Text("34")
                 }
+                Button(action: {
+                    self.searchBy = "36"
+                    self.expand.toggle()
+                }) {
+                    Text("36")
+                }
+                
+//                Button(action: {
+//                    self.searchBy = "userSize"
+//                    self.expand.toggle()
+//                }) {
+//                    Text("Your size")
+//                }
+//                Button(action: {
+//                    self.searchBy = "price"
+//                    self.expand.toggle()
+//                }) {
+//                    Text("Price €")
+//                }
             }
         }
     }
@@ -57,10 +76,16 @@ struct SearchView: View {
                     self.dropDown
                     List {
                         ForEach(networkingManager.clothingList.items) { item in
-                            if (self.searchText.isEmpty) {
+                            //self.searchBy
+                            if (self.searchBy.count > 0) {
+                                if (item.size.contains(self.searchBy)) {
+                                    SearchNavigation(item: item)
+                                }
+                            }
+                            if (self.searchText.isEmpty && self.searchBy.count == 0) {
                                 SearchNavigation(item: item)
                             }
-                            if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                            if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBy.count == 0) {
                                 SearchNavigation(item: item)
                             }
                         }
