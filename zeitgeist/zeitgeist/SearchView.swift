1668617aed6536001d582cff35c78d832c9aaaab
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-18 04:26:05 +0300

    Added dropdown menu to searchView

--- zeitgeist/zeitgeist/SearchView.swift
+++ zeitgeist/zeitgeist/SearchView.swift
@@ -14,11 +14,46 @@ struct SearchView: View {
     @ObservedObject var shoppingHistory = ShoppingHistory()
     @ObservedObject var networkingManager = NetworkingManager()
     @State private var searchText : String = ""
+    
+    @State var expand = false
+    
+    var dropDown: some View {
+        VStack(spacing: 30) {
+            HStack {
+                Text("text1")
+                Image(systemName: expand ? "chevron.up" : "chevron.down")
+            }.onTapGesture {
+                self.expand.toggle()
+            }
+            if expand {
+                Button(action: {
+                    print("333")
+                    self.expand.toggle()
+                }) {
+                    Text("aaa")
+                }
+                Button(action: {
+                    print("222")
+                    self.expand.toggle()
+                }) {
+                    Text("bbb")
+                }
+                Button(action: {
+                    print("111")
+                    self.expand.toggle()
+                }) {
+                    Text("bbb")
+                }
+            }
+        }
+    }
+    
     var body: some View {
         ZStack(alignment: .leading) {
             NavigationView {
                 VStack {
                     SearchBar(text: $searchText, placeholder: "searchItemsText")
+                    self.dropDown
                     List {
                         ForEach(networkingManager.clothingList.items) { item in
                             if (self.searchText.isEmpty) {
