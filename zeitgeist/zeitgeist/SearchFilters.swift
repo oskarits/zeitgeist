22c2ceb8ffd8db36491d6e5098f42f54e7e57145
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-18 19:09:45 +0300

    SearchView spliced with binding variables

---
+++ zeitgeist/zeitgeist/SearchFilters.swift
@@ -0,0 +1,68 @@
+//
+//  SearchFilters.swift
+//  zeitgeist
+//
+//  Created by Jari Pietikäinen on 18.4.2020.
+//  Copyright © 2020 Z Team. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchFilters: View {
+    @ObservedObject var shoppingHistory = ShoppingHistory()
+    @ObservedObject var networkingManager = NetworkingManager()
+    @Binding var searchText : String
+    @Binding var expand : Bool
+    @Binding var expand2 : Bool
+    @Binding var searchBySize : String
+    @Binding var searchByPrice : String
+    @State var sizes = ["One Size", "32", "34", "36", "38", "40", "42", "44"]
+    @State var prices = stride(from: 10, through: 80, by: 10).map(String.init)
+    @Binding var sizeFilterTitle : String
+    @Binding var priceFilterTitle : String
+    
+    var body: some View {
+        List {
+            ForEach(networkingManager.clothingList.items) { item in
+                
+                if (Int(item.price) ?? 99 <= Int(self.searchByPrice) ?? 0 && Int(item.price) ?? 99 >= (Int(self.searchByPrice) ?? 0) - 9) {
+                    
+                    if (self.searchText.isEmpty) {
+                        if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                            SearchNavigation(item: item)
+                            
+                        }
+                        if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
+                            SearchNavigation(item: item)
+                        }
+                    }
+                    if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                        if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                            SearchNavigation(item: item)
+                        }
+                        if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
+                            SearchNavigation(item: item)
+                        }
+                    }
+                }
+                if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
+                    if (item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                        if (self.searchText.isEmpty) {
+                            SearchNavigation(item: item)
+                        }
+                        if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                            SearchNavigation(item: item)
+                        }
+                    }
+                }
+                if (self.searchText.isEmpty && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
+                    SearchNavigation(item: item)
+                }
+                if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
+                    SearchNavigation(item: item)
+                }
+            }
+        }
+        
+    }
+}
--- zeitgeist/zeitgeist/SearchView.swift
+++ zeitgeist/zeitgeist/SearchView.swift
@@ -27,7 +27,7 @@ struct SearchView: View {
         ZStack(alignment: .leading) {
             NavigationView {
                 VStack {
-                    SearchBar(text: $searchText, placeholder: "searchItemsText")
+                    SearchBar(text: $searchText, placeholder: "Search by brand: ")
                     VStack {
                         HStack {
                             Spacer()
@@ -39,47 +39,7 @@ struct SearchView: View {
                             Spacer()
                         }
                     }
-                    List {
-                        ForEach(networkingManager.clothingList.items) { item in
-                            Aaa(item: item, searchText: self.$searchText, expand: self.$expand, expand2: self.$expand2, searchBySize: self.$searchBySize, searchByPrice: self.$searchByPrice, sizeFilterTitle: self.$sizeFilterTitle, priceFilterTitle: self.$priceFilterTitle)
-//                            if (Int(item.price) ?? 99 <= Int(self.searchByPrice) ?? 0 && Int(item.price) ?? 99 >= (Int(self.searchByPrice) ?? 0) - 9) {
-//
-//                                if (self.searchText.isEmpty) {
-//                                    if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-//                                        SearchNavigation(item: item)
-//
-//                                    }
-//                                    if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-//                                        SearchNavigation(item: item)
-//                                    }
-//                                }
-//                                if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-//                                    if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-//                                        SearchNavigation(item: item)
-//                                    }
-//                                    if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-//                                        SearchNavigation(item: item)
-//                                    }
-//                                }
-//                            }
-//                            if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
-//                                if (item.size.lowercased().contains(self.searchBySize.lowercased())) {
-//                                    if (self.searchText.isEmpty) {
-//                                        SearchNavigation(item: item)
-//                                    }
-//                                    if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-//                                        SearchNavigation(item: item)
-//                                    }
-//                                }
-//                            }
-//                            if (self.searchText.isEmpty && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
-//                                SearchNavigation(item: item)
-//                            }
-//                            if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
-//                                SearchNavigation(item: item)
-//                            }
-                        }
-                    }
+                    SearchFilters(searchText: self.$searchText, expand: self.$expand, expand2: self.$expand2, searchBySize: self.$searchBySize, searchByPrice: self.$searchByPrice, sizeFilterTitle: self.$sizeFilterTitle, priceFilterTitle: self.$priceFilterTitle)
                 }
                 .frame(
                     minWidth: 0,
@@ -98,69 +58,9 @@ struct SearchView: View {
         }.resignKeyboardOnDragGesture()
     }
 }
-struct Aaa: View {
-    var item : ClothingListEntry
-    @ObservedObject var shoppingHistory = ShoppingHistory()
-    @ObservedObject var networkingManager = NetworkingManager()
-    @Binding var searchText : String
-    @Binding var expand : Bool
-    @Binding var expand2 : Bool
-    @Binding var searchBySize : String
-    @Binding var searchByPrice : String
-    @State var sizes = ["One Size", "32", "34", "36", "38", "40", "42", "44"]
-    @State var prices = stride(from: 10, through: 80, by: 10).map(String.init)
-    @Binding var sizeFilterTitle : String
-    @Binding var priceFilterTitle : String
 
-    var body: some View {
-
-        VStack {
-            if (Int(item.price) ?? 99 <= Int(self.searchByPrice) ?? 0 && Int(item.price) ?? 99 >= (Int(self.searchByPrice) ?? 0) - 9) {
-                
-                if (self.searchText.isEmpty) {
-                    if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                        SearchNavigation(item: item)
-                        
-                    }
-                    if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-                        SearchNavigation(item: item)
-                    }
-                }
-                if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-                    if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                        SearchNavigation(item: item)
-                    }
-                    if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-                        SearchNavigation(item: item)
-                    }
-                }
-            }
-            if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
-                if (item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                    if (self.searchText.isEmpty) {
-                        SearchNavigation(item: item)
-                    }
-                    if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-                        SearchNavigation(item: item)
-                    }
-                }
-            }
-            if (self.searchText.isEmpty && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
-                SearchNavigation(item: item)
-            }
-            if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
-                SearchNavigation(item: item)
-            }
-        }
-        
-    }
-}
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
     }
 }
-
-/*
- Aaa(item: item, searchText: self.$searchText, expand: self.$expand, expand2: self.$expand2, searchBySize: self.$searchBySize, searchByPrice: self.$searchByPrice, sizeFilterTitle: self.$sizeFilterTitle, priceFilterTitle: self.$priceFilterTitle)
- */
