0be04783405770460eae516f04e3c2ab905ea676
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-19 20:00:52 +0300

    Search filter by user size

--- zeitgeist/zeitgeist/SearchFilters.swift
+++ zeitgeist/zeitgeist/SearchFilters.swift
@@ -7,9 +7,11 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct SearchFilters: View {
-    //@ObservedObject var shoppingHistory = ShoppingHistory()
+    @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
+    @FetchRequest(fetchRequest: LoginNode.getNodes()) var isLoggedInResults: FetchedResults<LoginNode>
     @ObservedObject var networkingManager = NetworkingManager()
     @Binding var searchText : String
     @Binding var expand : Bool
@@ -30,19 +32,33 @@ struct SearchFilters: View {
                     if (self.searchText.isEmpty) {
                         if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
                             SearchNavigation(item: item)
-                            
                         }
                         if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
                             SearchNavigation(item: item)
                         }
+                        if (self.searchBySize == "Your size") {
+                            if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                                SearchNavigation(item: item)
+                            }
+                        }
                     }
-                    if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                    if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize != "Your size") {
                         if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
                             SearchNavigation(item: item)
                         }
                         if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
                             SearchNavigation(item: item)
                         }
+                    }
+                    if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size") {
+                        if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                            SearchNavigation(item: item)
+                        }
+                    }
+                }
+                if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size" && self.searchByPrice.count < 1) {
+                    if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                        SearchNavigation(item: item)
                     }
                 }
                 if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
@@ -51,6 +67,11 @@ struct SearchFilters: View {
                             SearchNavigation(item: item)
                         }
                         if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                            SearchNavigation(item: item)
+                        }
+                    }
+                    if (self.searchBySize == "Your size" && self.searchText.isEmpty) {
+                        if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
                             SearchNavigation(item: item)
                         }
                     }
@@ -63,6 +84,5 @@ struct SearchFilters: View {
                 }
             }
         }
-        
     }
 }
