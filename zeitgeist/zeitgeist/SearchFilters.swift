d1887586dd25756de968906a2f9556ae2fd200e8
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-20 15:40:55 +0300

    Search filter bug fix

--- zeitgeist/zeitgeist/SearchFilters.swift
+++ zeitgeist/zeitgeist/SearchFilters.swift
@@ -37,20 +37,50 @@ struct SearchFilters: View {
                     }
                 }
             }// if empty
-            ForEach(networkingManager.clothingList.items) { item in
-                
-                if (Int(item.price) ?? 99 <= Int(self.searchByPrice) ?? 0 && Int(item.price) ?? 99 >= (Int(self.searchByPrice) ?? 0) - 9) {
-                    
-                    if (self.searchText.isEmpty) {
-                        if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
+            if (self.searchText.isEmpty == false || self.searchBySize.count > 0 || self.searchByPrice.count > 0) {
+                List {
+                    ForEach(networkingManager.clothingList.items) { item in
+                        
+                        if (Int(item.price) ?? 99 <= Int(self.searchByPrice) ?? 0 && Int(item.price) ?? 99 >= (Int(self.searchByPrice) ?? 0) - 9) {
+                            
+                            if (self.searchText.isEmpty) {
+                                if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                                if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                                if (self.searchBySize == "Your size") {
+                                    if (self.isLoggedInResults.endIndex > 0) {
+                                        if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                                            ImageView(item: item)
+                                            SearchNavigation(item: item)
+                                        }
+                                    }
+                                }
+                            }
+                            if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize != "Your size") {
+                                if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                                if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                            }
+                            if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size") {
+                                if (self.isLoggedInResults.endIndex > 0) {
+                                    if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                                        ImageView(item: item)
+                                        SearchNavigation(item: item)
+                                    }
+                                }
+                            }
                         }
-                        if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
-                        }
-                        if (self.searchBySize == "Your size") {
+                        if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size" && self.searchByPrice.count < 1) {
                             if (self.isLoggedInResults.endIndex > 0) {
                                 if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
                                     ImageView(item: item)
@@ -58,57 +88,31 @@ struct SearchFilters: View {
                                 }
                             }
                         }
-                    }
-                    if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize != "Your size") {
-                        if (Int(self.searchByPrice) ?? 0 >= 5 && item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
-                        }
-                        if (Int(self.searchByPrice) ?? 0 >= 5 && self.searchBySize.count == 0) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
-                        }
-                    }
-                    if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size") {
-                        if (self.isLoggedInResults.endIndex > 0) {
-                            if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
-                                ImageView(item: item)
-                                SearchNavigation(item: item)
+                        if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
+                            if (item.size.lowercased().contains(self.searchBySize.lowercased())) {
+                                if (self.searchText.isEmpty) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                                if (item.brand.lowercased().contains(self.searchText.lowercased())) {
+                                    ImageView(item: item)
+                                    SearchNavigation(item: item)
+                                }
+                            }
+                            if (self.searchBySize == "Your size" && self.searchText.isEmpty) {
+                                if (self.isLoggedInResults.endIndex > 0) {
+                                    if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                                        ImageView(item: item)
+                                        SearchNavigation(item: item)
+                                    }
+                                }
                             }
                         }
-                    }
-                }
-                if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize == "Your size" && self.searchByPrice.count < 1) {
-                    if (self.isLoggedInResults.endIndex > 0) {
-                        if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
+                        if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
                             ImageView(item: item)
                             SearchNavigation(item: item)
                         }
                     }
-                }
-                if (self.searchBySize.count > 0 && self.searchByPrice.count == 0) {
-                    if (item.size.lowercased().contains(self.searchBySize.lowercased())) {
-                        if (self.searchText.isEmpty) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
-                        }
-                        if (item.brand.lowercased().contains(self.searchText.lowercased())) {
-                            ImageView(item: item)
-                            SearchNavigation(item: item)
-                        }
-                    }
-                    if (self.searchBySize == "Your size" && self.searchText.isEmpty) {
-                        if (self.isLoggedInResults.endIndex > 0) {
-                            if (item.size.lowercased().contains(self.isLoggedInResults[0].size)) {
-                                ImageView(item: item)
-                                SearchNavigation(item: item)
-                            }
-                        }
-                    }
-                }
-                if (item.brand.lowercased().contains(self.searchText.lowercased()) && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
-                    ImageView(item: item)
-                    SearchNavigation(item: item)
                 }
             }
         }//VStack view
