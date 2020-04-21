3addfdc6e7da761fae49b2ce9212249b757ca5d3
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-04-21 05:00:19 +0300

    ShoppingHistory checks if logged in & removed commented code

--- zeitgeist/zeitgeist/NavigationBarView.swift
+++ zeitgeist/zeitgeist/NavigationBarView.swift
@@ -24,34 +24,24 @@ struct NavigationBarView: View {
     
     var body: some View {
         TabView {
-            //SignInView()
             HomeView()
-            //TabBar()
                 .tabItem {
                     Image(systemName: "house").font(Font.system(size: 30, weight: .regular))
             }
             if isLoggedInResults.isEmpty {
                 SignInView().tabItem {
-                        Image(systemName: "person").font(Font.system(size: 30, weight: .regular))
+                    Image(systemName: "person").font(Font.system(size: 30, weight: .regular))
                 }
             } else {
                 ProfileView().tabItem {
-                        Image(systemName: "person").font(Font.system(size: 30, weight: .regular))
+                    Image(systemName: "person").font(Font.system(size: 30, weight: .regular))
                 }
             }
-            //SearchView()
             TabBar()
                 .tabItem {
                     Image(systemName: "magnifyingglass").font(Font.system(size: 30, weight: .regular))
             }
-            //ItemNodeView()
-            
-            
-                
-            
-            //ItemNodeView()
             ShoppingHistory()
-            //QrView()
                 .tabItem {
                     Image(systemName: "barcode").font(Font.system(size: 30, weight: .regular))
             }
@@ -60,11 +50,11 @@ struct NavigationBarView: View {
                     Image("zircle").resizable().frame(width: 30, height: 30)}
         }
     }
-
-
-struct NavigationBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationBarView()
+    
+    
+    struct NavigationBarView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationBarView()
+        }
     }
-}
 }
