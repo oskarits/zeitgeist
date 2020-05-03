39486eaf708f896e0b73eff04dee24103245580f
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-05-03 21:57:37 +0300

    SearchFilters comments for State and Binding variables

--- zeitgeist/zeitgeist/SearchFilters.swift
+++ zeitgeist/zeitgeist/SearchFilters.swift
@@ -9,23 +9,35 @@
 import SwiftUI
 import CoreData
 import WaterfallGrid
+
 struct SearchFilters: View {
+    // Allows the use of core data
     @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
+    // Fetches core data using LoginNode NSManagedObject class
     @FetchRequest(fetchRequest: LoginNode.getNodes()) var isLoggedInResults: FetchedResults<LoginNode>
+    // Fetches data from URL in NetworkingManager ObservableObject class
     @ObservedObject var networkingManager = NetworkingManager()
+    // Binding value for searchbar
     @Binding var searchText : String
+    // Binding value for expanding size filter
     @Binding var expand : Bool
+    // Binding value for expanding price filter
     @Binding var expand2 : Bool
+    // Binding value for string value of searched size
     @Binding var searchBySize : String
+    // Binding value for string value of searched price
     @Binding var searchByPrice : String
+    // Selectable sizes to filter
     @State var sizes = ["Your size", "One Size", "32", "34", "36", "38", "40", "42", "44"]
+    // Selectable prices to filter
     @State var prices = stride(from: 10, through: 80, by: 10).map(String.init)
+    // Binding value for string value of searched size used in Text()
     @Binding var sizeFilterTitle : String
+    // Binding value for string value of searched price used in Text()
     @Binding var priceFilterTitle : String
     
     var body: some View {
         VStack {
-            
             if (self.searchText.isEmpty && self.searchBySize.count == 0 && self.searchByPrice.count == 0) {
                 VStack {
                     WaterfallGrid(self.networkingManager.clothingList.items) {item in
