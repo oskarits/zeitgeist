b5b868879faeed98f53175a0a80174da0b3e9aeb
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-05-04 00:06:47 +0300

    Toast comments

--- zeitgeist/zeitgeist/Toast.swift
+++ zeitgeist/zeitgeist/Toast.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 
 struct ToastView<Content>: View where Content: View {
-    
+    // Binding variable to show 'Toast'
     @Binding var show: Bool
+    // String for the 'Toast'
     let text: String
     var overlaidContent: () -> Content
     
@@ -18,24 +19,22 @@ struct ToastView<Content>: View where Content: View {
         GeometryReader { geometry in
             ZStack(alignment: .bottom) {
                 self.overlaidContent()
-                
                 if self.show {
                     HStack {
                         Text("item1").foregroundColor(Color.white)
                         Text(self.text)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color.orange)
-                        //.padding()
                         Text("item2").foregroundColor(Color.white)
                     }.padding()
                         .frame(width: geometry.size.width < geometry.size.height ? geometry.size.width * 0.85 : geometry.size.width * 0.65)
-                        //.background(Color(UIColor.secondarySystemBackground))
                         .background(Color(UIColor.darkGray))
                         .cornerRadius(20)
                         .padding([.vertical], 15)
                         .animation(.easeInOut(duration: 1.0))
                         .transition(.move(edge: .bottom))
                         .onAppear {
+                            // Async to hide the 'Toast' view
                             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                                 self.show.toggle()
                             }
@@ -45,7 +44,7 @@ struct ToastView<Content>: View where Content: View {
         }
     }
 }
-
+// Called when showing 'Toast'
 extension View {
     func toast(show: Binding<Bool>, text: String) -> some View {
         ToastView(show: show, text: text) {
