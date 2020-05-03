7c3ce1499131c0770e2065bdd102a2e892546521
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2020-05-03 18:00:04 +0300

    added purchase date and formatted date

--- zeitgeist/zeitgeist/ShoppingHistoryView.swift
+++ zeitgeist/zeitgeist/ShoppingHistoryView.swift
@@ -16,16 +16,38 @@ struct ShoppingHistoryView: View {
     @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
     @FetchRequest(fetchRequest: CheckoutNode.getNodes()) var checkoutResults: FetchedResults<CheckoutNode>
     
+    
     var body: some View {
         List(checkoutResults, id: \.self) { node in
-            VStack {
-                Text(node.brand)
-                Text(node.size)
-                Text(node.price)
+            HStack {
+                VStack {
+                    Text(node.brand)
+                        .fontWeight(.medium)
+                        .font(.system(size: 18))
+                    Text(node.size)
+                        .font(.system(size: 11))
+                    Text(node.price)
+                        .font(.system(size: 11))
+                        .foregroundColor(.orange)
+                }
+                .padding()
+                HStack {
+                    Text("")
+                    Text("\(formatingDate)")
+                }
             }
         }
     }
 }
+
+func getFormattedDate(date: Date, format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: date)
+}
+
+let formatingDate = getFormattedDate(date: Date(), format: "dd.MM.yy")
+       
 
 struct ShoppingHistoryView_Previews: PreviewProvider {
     static var previews: some View {
