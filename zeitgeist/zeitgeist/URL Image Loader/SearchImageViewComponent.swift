a5c7ed0e9e43c2fd425c67af0b338e738774bf61
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-05-04 00:48:55 +0300

    SearchImageViewComponent comments

--- zeitgeist/zeitgeist/URL Image Loader/SearchImageViewComponent.swift
+++ zeitgeist/zeitgeist/URL Image Loader/SearchImageViewComponent.swift
@@ -9,18 +9,18 @@
 import SwiftUI
 
 struct SearchImageViewComponent: View {
-        
-        @ObservedObject var imageLoader: ImageLoader
-        
-        init(url: String) {
-            imageLoader = ImageLoader(url: url)
-        }
-         
-        var body: some View {
-            VStack {
+    // Placeholder for ObservableObject ImageLoader
+    @ObservedObject var imageLoader: ImageLoader
+    // Initialising item image URL
+    init(url: String) {
+        imageLoader = ImageLoader(url: url)
+    }
+    
+    var body: some View {
+        VStack { // Place image from URL and nil guards
             Image(uiImage: ((imageLoader.data.count == 0) ? UIImage(named: "logoapple")! : UIImage(data: imageLoader.data)) ?? UIImage(systemName: "house")!)
                 .resizable().scaledToFit()
-            }.frame(minWidth: 150, maxWidth: 175, minHeight: 150, maxHeight: 250)
-        }
-        
+        }.frame(minWidth: 150, maxWidth: 175, minHeight: 150, maxHeight: 250) // Size for image frame
     }
+    
+}
