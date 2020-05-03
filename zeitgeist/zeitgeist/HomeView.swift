d35463ad5f500a5b6ec185f5704e95aed6d34818
Jari Pietikäinen <jari.pietikainen@metropolia.fi>
2020-05-03 19:07:12 +0300

    HomeView scroll

--- zeitgeist/zeitgeist/HomeView.swift
+++ zeitgeist/zeitgeist/HomeView.swift
@@ -20,69 +20,71 @@ struct HomeView: View {
     var body: some View {
         
         VStack(alignment: .leading) {
-            if isLoggedInResults.isEmpty {
-                VStack() {
-                    Divider()
-                    Text("notificationTextTitle")
-                        .fontWeight(.bold)
-                        .foregroundColor(.red)
-                    Text("notificationText")
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    Divider()
-                    HStack {
-                        Text("Sign in to share your")
-                            .fontWeight(.bold)
-                        Text("shareTitle2")
-                            .foregroundColor(.orange)
-                            .fontWeight(.bold)
-                    }.padding(50)
-                    Image("zalandoCampaignBlurred")
-                        .resizable()
-                        .frame(width: 380, height: 240)
-                        .padding()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Text("Store Locator")
-                            .foregroundColor(Color.white)
-                             .font(.title)
-                             .frame(width: 300, height: 80)
-                             .background(Color.orange)
-                             .cornerRadius(15.0)
-                    }
-                }.frame(alignment: .topLeading)
-                Spacer()
-            } else {
-                VStack(alignment: .center) {
-                    Spacer()
-                    VStack {
+            ScrollView{
+                if isLoggedInResults.isEmpty {
+                    VStack() {
+                        Divider()
                         Text("notificationTextTitle")
                             .fontWeight(.bold)
-
                             .foregroundColor(.red)
                         Text("notificationText")
-                        }.padding(.top).padding()
-
-                    VStack {
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        Divider()
                         HStack {
-                            
-                            Text("shareTitle1")
-                                .font(.title)
-                            Text("shareTitle2")
-                                .font(.title)
+                            Text("Sign in to share your")
                                 .fontWeight(.bold)
+                            Text("shareTitle2")
                                 .foregroundColor(.orange)
+                                .fontWeight(.bold)
+                        }.padding(50)
+                        Image("zalandoCampaignBlurred")
+                            .resizable()
+                            .frame(width: 380, height: 240)
+                            .padding()
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("Store Locator")
+                                .foregroundColor(Color.white)
+                                .font(.title)
+                                .frame(width: 300, height: 80)
+                                .background(Color.orange)
+                                .cornerRadius(15.0)
                         }
-                        QRMaker()
-                    }
-                    VStack {
-                        List(networkManager.courses) { course in
-                            HStack(alignment: .center) {
-                                Spacer()
+                    }.frame(alignment: .topLeading)
+                    Spacer()
+                } else {
+                    VStack(alignment: .center) {
+                        Spacer()
+                        VStack {
+                            Text("notificationTextTitle")
+                                .fontWeight(.bold)
                                 
-                                CampaignView(course: course)
-                                Spacer()
+                                .foregroundColor(.red)
+                            Text("notificationText")
+                        }.padding(.top).padding()
+                        
+                        VStack {
+                            HStack {
+                                
+                                Text("shareTitle1")
+                                    .font(.title)
+                                Text("shareTitle2")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.orange)
                             }
-
+                            VStack {
+                                QRMaker()
+                            }.frame(maxWidth: 200, maxHeight: 200)
+                        }
+                        VStack {
+                            ForEach(networkManager.courses) { course in
+                                HStack(alignment: .center) {
+                                    Spacer()
+                                    CampaignView(course: course)
+                                    Spacer()
+                                }
+                            }
                         }
                     }
                 }
@@ -90,8 +92,6 @@ struct HomeView: View {
         }
     }
 }
-
-
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
